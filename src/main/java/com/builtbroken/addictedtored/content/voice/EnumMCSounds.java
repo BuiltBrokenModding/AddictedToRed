0fb2d5f4dbd2e7cdbb851c339abb4940ac065edd
Robert S <rseifert.phone@gmail.com>
2015-03-06 04:38:59 -0500

    [Errors] Started entering category to name for sound enum

--- src/main/java/com/builtbroken/addictedtored/content/voice/EnumMCSounds.java
+++ src/main/java/com/builtbroken/addictedtored/content/voice/EnumMCSounds.java
@@ -1,44 +1,46 @@
 package com.builtbroken.addictedtored.content.voice;
 
+import java.util.HashMap;
+
 /**
  * Created by robert on 3/6/2015.
  */
 public enum EnumMCSounds
 {
     //TODO move to core later
-    VILLAGER_YES("mob.villager.yes"),
-    VILLAGER_NO("mob.villager.no"),
-    VILLAGER_HAGGLE("mob.villager.haggle"),
-    VILLAGER_IDLE("mob.villager.idle"),
-    VILLAGER_HIT("mob.villager.hit"),
-    VILLAGER_DEATH("mob.villager.death"),
-    CHICKEN_SAY("mob.chicken.say"),
-    CHICKEN_HURT("mob.chicken.hurt"),
-    CHICKEN_STEP("mob.chicken.step"),
-    COW_SAY("mob.cow.say"),
-    COW_HURT("mob.cow.hurt"),
-    COW_STEP("mob.cow.step"),
-    HORSE_LAND("mob.horse.land"),
-    HORSE_ARMOR("mob.horse.armor"),
-    HORSE_LEATHER("mob.horse.leather"),
-    HORSE_ZOMBIE_DEATH("mob.horse.zombie.death"),
-    HORSE_SKELETON_DEATH("mob.horse.skeleton.death"),
-    HORSE_DEATH("mob.horse.death"),
-    DONKEY_DEATH("mob.horse.donkey.death"),
-    HORSE_ZOMBIE_HIT("mob.horse.zombie.hit"),
-    HORSE_SKELETON_HIT("mob.horse.skeleton.hit"),
-    HORSE_HIT("mob.horse.hit"),
-    DONKEY_HIT("mob.horse.donkey.hit"),
-    HORSE_ZOMBIE_IDLE("mob.horse.zombie.idle"),
-    HORSE_SKELETON_IDLE("mob.horse.skeleton.idle"),
-    HORSE_IDLE("mob.horse.idle"),
-    DONKEY_IDLE("mob.horse.donkey.idle"),
-    HORSE_ANGRY("mob.horse.angry"),
-    DONKEY_ANGRY("mob.horse.donkey.angry"),
-    HORSE_GALLOP("mob.horse.gallop"),
-    HORSE_BREATHE("mob.horse.breathe"),
-    HORSE_STEP_WOOD("mob.horse.wood"),
-    HORSE_STEP_SOFT("mob.horse.soft"),
+    VILLAGER_YES("mob.villager.yes", "villager", "yes"),
+    VILLAGER_NO("mob.villager.no", "villager", "no"),
+    VILLAGER_HAGGLE("mob.villager.haggle", "villager", "haggle"),
+    VILLAGER_IDLE("mob.villager.idle", "villager", "idle"),
+    VILLAGER_HIT("mob.villager.hit", "villager", "hit"),
+    VILLAGER_DEATH("mob.villager.death", "villager", "death"),
+    CHICKEN_SAY("mob.chicken.say", "chicken", "say"),
+    CHICKEN_HURT("mob.chicken.hurt", "chicken", "hurt"),
+    CHICKEN_STEP("mob.chicken.step", "chicken", "step"),
+    COW_SAY("mob.cow.say", "cow", "say"),
+    COW_HURT("mob.cow.hurt", "cow", "hurt"),
+    COW_STEP("mob.cow.step", "cow", "step"),
+    HORSE_LAND("mob.horse.land", "horse", "land"),
+    HORSE_ARMOR("mob.horse.armor", "horse", "armor"),
+    HORSE_LEATHER("mob.horse.leather", "horse", "saddle"),
+    HORSE_ZOMBIE_DEATH("mob.horse.zombie.death", "zombie horse", "death"),
+    HORSE_SKELETON_DEATH("mob.horse.skeleton.death", "skeleton horse", "death"),
+    HORSE_DEATH("mob.horse.death", "horse", "death"),
+    DONKEY_DEATH("mob.horse.donkey.death", "donkey", "death"),
+    HORSE_ZOMBIE_HIT("mob.horse.zombie.hit", "zombie horse", "hit"),
+    HORSE_SKELETON_HIT("mob.horse.skeleton.hit", "skeleton horse", "hit"),
+    HORSE_HIT("mob.horse.hit", "horse", "hit"),
+    DONKEY_HIT("mob.horse.donkey.hit", "donkey", "death"),
+    HORSE_ZOMBIE_IDLE("mob.horse.zombie.idle", "zombie horse", "idle"),
+    HORSE_SKELETON_IDLE("mob.horse.skeleton.idle", "skeleton horse", "idle"),
+    HORSE_IDLE("mob.horse.idle", "horse", "idle"),
+    DONKEY_IDLE("mob.horse.donkey.idle", "donkey", "idle"),
+    HORSE_ANGRY("mob.horse.angry", "horse", "angry"),
+    DONKEY_ANGRY("mob.horse.donkey.angry", "donkey", "angry"),
+    HORSE_GALLOP("mob.horse.gallop", "horse", "gallop"),
+    HORSE_BREATHE("mob.horse.breathe", "horse", "breathe"),
+    HORSE_STEP_WOOD("mob.horse.wood", "horse", "step wood"),
+    HORSE_STEP_SOFT("mob.horse.soft", "horse", "step soft"),
     PIG_SAY("mob.pig.say"),
     PIG_DEATH("mob.pig.death"),
     PIG_STEP("mob.pig.step"),
@@ -85,20 +87,33 @@ public enum EnumMCSounds
     //TODO add skeleton, slime, snowman, spider, zombie, wither, ender dragon, lava, fire, and random sounds
 
 
-
+    public final String sound;
+    public final String cat;
     public final String name;
     public final float volume;
     public final float pitch;
 
-    private EnumMCSounds(String name)
+    private static boolean built = false;
+
+    //List of sounds by category for sorting in GUIs <Category, <Name, Sound String or Enum Entry>>
+    public final HashMap<String, HashMap<String, Object>> catToSoundMap = new HashMap();
+
+    private EnumMCSounds(String sound, String cat, String name)
     {
-        this(name, 1.0f, 1.0f);
+        this(sound, cat, name, 1.0f, 1.0f);
     }
 
-    private EnumMCSounds(String name, float volume, float pitch)
+    private EnumMCSounds(String sound, String cat, String name, float volume, float pitch)
     {
+        this.sound = sound;
+        this.cat = cat;
         this.name = name;
         this.volume = volume;
         this.pitch = pitch;
+        if(!catToSoundMap.containsKey(cat))
+        {
+            catToSoundMap.put(cat, new HashMap());
+        }
+        catToSoundMap.get(cat).put(name, this);
     }
 }
